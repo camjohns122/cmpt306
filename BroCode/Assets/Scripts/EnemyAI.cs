a7b32a258beb3389b80b4411ef340723f697c114
hustone <ethanj.houston@gmail.com>
2016-10-11 19:38:45 -0600

    Scene
    I added my Scene.
    
    Also added the Chicken Sprite and using it for the Enemy AI, Script
    unfinished but I just wanted to commit and Sync first.

---
+++ BroCode/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    private GameObject Baddy;
+    private GameObject Player;
+    public float Range;
+    [SerializeField]
+    public float Speed;
+    //public bool moveRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        Baddy = GameObject.FindGameObjectWithTag("Enemy");
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Range = Vector2.Distance(Baddy.transform.position, Player.transform.position);
+
+        if (Range <= 40f)
+        {
+            transform.Translate(Vector2.MoveTowards(Baddy.transform.position, Player.transform.position, Range) * Speed * Time.deltaTime);
+        }
+    }
+}
