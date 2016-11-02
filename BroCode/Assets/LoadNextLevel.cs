60900509bc408ae33957cdbe8628b1e88d5e38ea
Brin Surtie <brin.surtie@shaw.ca>
2016-11-02 01:26:31 -0600

    Progressing from Level 1 to Boss Battle 1
    I made it so the player starts the level at a barn.  The player then
    ends the level at another barn.  Upon reaching the end barn, the player
    is transported to the boss battle.
    
    I also made slight modifications to the player and the bosses box
    collider size.

---
+++ BroCode/Assets/LoadNextLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script is used to load the next level.
+public class LoadNextLevel : MonoBehaviour
+{
+	// Type in the inspector which level you want to load.
+	public string levelToLoad;
+
+	// Function to tell if the player is at the end of the level
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			Application.LoadLevel (levelToLoad);
+		}
+	}
+}
