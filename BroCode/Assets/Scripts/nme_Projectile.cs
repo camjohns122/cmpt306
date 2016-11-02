c2b779a726d2b00de3ffc2effc3ebb031e4ab872
hustone <ethanj.houston@gmail.com>
2016-11-02 16:04:04 -0600

    Made A Big Chicken
    This is just another alternative Enemy I call Big Chicken. It's a Big
    Chicken. So like a chicken... but bigger. It just patrols for right now
    and doesn't flip. But, when the player is close enough it shoots an Egg.
    For the Egg I made it a new prefab called nme_Projectile, it damages the
    player as is destroyed on collision with player or player projectile.
    Some fire rate balancing is probably needed.

---
+++ BroCode/Assets/Scripts/nme_Projectile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nme_Projectile : MonoBehaviour {
+
+	public float speed;					// Speed of projectile.
+	public BigChicken bigChicken;			// Projectile direction is based on the Player's direction.
+
+	// Make sure to attach confidence.
+	public GameObject confidence;
+
+	// The amount of confidence you lose when you get hit by a chicken.
+	public float confidenceToLose;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Finds the Object that has a Controller script attached to it (Player)
+		bigChicken = FindObjectOfType<BigChicken> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		// Make the projectile move horizontally.
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, GetComponent<Rigidbody2D>().velocity.y);﻿
+	}
+
+	// Destroy the projectile when it makes contact with something.
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the Egg hits the player, the player loses confidence.
+		if (col.gameObject.tag == "Player")
+		{
+			confidence.GetComponent<Confidence>().loseConfidence (confidenceToLose);
+		}
+
+		// If the Egg gets hit by the player's projectile, destroys both.
+		if (col.gameObject.tag == "Projectile")
+		{
+			Destroy (gameObject);
+		}
+	}
+}
