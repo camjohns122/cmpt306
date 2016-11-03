c9d050388ac829f1ce5aadde9574f010ecb35e9a
HotDamnGotDamn <Kevin@kevins-mbp.gateway.pace.com>
2016-11-02 21:57:53 -0600

    Pig charges at player
    Made a pig game object with a script attached. When the pig is close to
    the player its speed will increase as it attacks/runs towards the
    player. If the pig is out of range, the speed will go back to normal.

---
+++ BroCode/Assets/Scripts/PigChargeAtPlayer.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigChargeAtPlayer : MonoBehaviour {
+
+	//Patrol Variables
+	private Vector3 PointA;
+
+	private Vector3 PointB;
+
+	[SerializeField]
+	private Transform Point1;
+
+	[SerializeField]
+	private Transform Point2;
+
+	private Vector3 next;
+
+	[SerializeField]
+	private float moveSpeed = 2f;
+
+
+
+	//Player Variables
+	private Controller Hero;
+
+	public LayerMask isPlayer;
+	public float HeroDistance;
+	public bool HeroNear;
+
+
+	// Use this for initialization
+	void Start () {
+		//Patrol Points initialized
+		PointA = Point1.localPosition;
+		PointB = Point2.localPosition;
+		next = PointB;
+
+		//Player grabbed
+		Hero = FindObjectOfType<Controller>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//check if Hero is in radius
+		HeroNear = Physics2D.OverlapCircle(transform.position, HeroDistance, isPlayer);
+
+		//if hero is near, speed increases while chasing. else patrol
+		if(HeroNear){
+			moveSpeed = 4f;
+			transform.position = Vector3.MoveTowards(transform.position, Hero.transform.position, moveSpeed * Time.deltaTime);
+		}
+		else{
+			moveSpeed = 2f;
+			Patrol();
+		}
+
+	}
+
+	//Move Enemy back and forth between two points
+	private void Patrol()
+	{
+		transform.localPosition = Vector3.MoveTowards(transform.localPosition, next, moveSpeed * Time.deltaTime);
+
+		if (Vector3.Distance(transform.localPosition, next) <= 0.1)
+		{
+			ChangeDestination();
+		}
+	}
+
+	//Cycle through patrol points
+	private void ChangeDestination()
+	{
+		next = next != PointA ? PointA : PointB;
+	}
+
+}
