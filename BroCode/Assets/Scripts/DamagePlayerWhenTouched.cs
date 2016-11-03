0b816a5ec3d0c9a005cd71fbe91053e346973e36
Brin Surtie <brin.surtie@shaw.ca>
2016-11-02 19:32:34 -0600

    Confidence has an impact on Player Damage and Boss Damage
    I have made it so the confidence actually does something useful.
    
    When the player has high confidence, they do high damage to the boss.
    However, the boss now does high damage to them.  As expect, players who
    are experts at this game will have high confidence and be able to
    tackle the challenge while dealing high damage.
    
    When the player has low confidence, they do a low amount of damage, but
    the boss will also deal a low amount of damage.  This is for when
    players are not good at the game.  The boss will have a hard time
    killing them, so they will have plenty of practice to shoot down the
    boss with their low damage.

--- BroCode/Assets/Scripts/DamagePlayerWhenTouched.cs
+++ BroCode/Assets/Scripts/DamagePlayerWhenTouched.cs
@@ -7,12 +7,17 @@ public class DamagePlayerWhenTouched : MonoBehaviour
 	// This is the amount of damage to give to the player.
 	public float damageToGive;
 
+	// Make sure to attach the confidence.
+	public GameObject confidence;
+
 	// When the object hits the player, subtract the apprioriate health from the player.
 	void OnTriggerEnter2D(Collider2D col)
 	{
+		// When the player has high confidence, the boss does high damage.
+		// When the player has low confidence, the boss does low damage.
 		if (col.gameObject.tag == "Player")
 		{
-			col.GetComponent<PlayerHealth> ().giveDamage (damageToGive);
+			col.GetComponent<PlayerHealth> ().giveDamage (1 + damageToGive * confidence.GetComponent<Confidence> ().getConfidence ()/25);
 		}
 	}
 }
--- BroCode/Assets/Scripts/PlayerProjectileDamage.cs
+++ BroCode/Assets/Scripts/PlayerProjectileDamage.cs
@@ -7,15 +7,19 @@ public class PlayerProjectileDamage : MonoBehaviour
 	// This is the amount of damage to give to the player.
 	public float damageToGive;
 
+	// Make sure to attach the confidence.
+	public GameObject confidence;
+
 	// When the object hits the player, subtract the apprioriate health from the player.
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "Enemy")
 		{
 			// If boss one exists, give damage to him.
+			// Add the amount of confidence to the damage given.
 			if (col.GetComponent<BossOneHealth> () != null)
 			{
-				col.GetComponent<BossOneHealth> ().giveDamage (damageToGive);
+				col.GetComponent<BossOneHealth> ().giveDamage (damageToGive + confidence.GetComponent<Confidence> ().getConfidence ()/25f);
 			}
 		}
 	}
