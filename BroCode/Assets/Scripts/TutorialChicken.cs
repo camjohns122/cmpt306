7310fc880fefc9af2ae9f23a2e0928c9fc1e6ba3
hustone <ethanj.houston@gmail.com>
2016-12-05 17:19:48 -0600

    ROUGH mockup Tutorial
    Super Basic Tutorial Type Screen
    
    Basically has text explaining basics, and you have to jump and shoot a
    chicken to show you know the controls. When the chicken is shot, loads
    Level 1.
    
    Basic and Ugly. Want to know what you guys think.

---
+++ BroCode/Assets/Scripts/TutorialChicken.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialChicken : MonoBehaviour {
+
+	// Type in the inspector which level you want to load.
+	public string levelToLoad;
+
+	public bool loadIt = false;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(loadIt){
+			Application.LoadLevel (levelToLoad);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		
+		if (col.gameObject.tag == "Projectile")
+		{
+			loadIt = true;
+		}
+	}
+
+}
