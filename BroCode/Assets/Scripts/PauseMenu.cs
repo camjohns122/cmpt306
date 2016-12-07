0b1c6617e8b25575206279bdf4090e5821f41d83
HotDamnGotDamn <Kevin@uofs-10-227-143-225.usask.ca>
2016-12-06 18:13:10 -0600

    Added pause menu to Level 1,2,3
    Made a pause menu prefab + script; added the prefab to level 1,2,3. The
    pause menu can either resume game or quit to main menu.
    
    The Esc key is used to pause the game.
    
    Also added the Start Screen scene to File -> Build Settings to get rid
    of that “Scene _____ couldn’t be loaded….” error.

---
+++ BroCode/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Specify what level to load in the Inspector
+	public string levelToLoad;
+
+	// Checks if game is paused or not
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Canvas is set active if paused, not active otherwise
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+
+			// when paused, set the time to 0
+			Time.timeScale = 0f;
+		} 
+		else {
+			pauseMenuCanvas.SetActive (false);
+
+			// when unpaused, resets the time to normal
+			Time.timeScale = 1f;
+		}
+
+		// If Esc key is pressed, it will either pause or resume
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+	}
+
+	// Resume playing the game
+	public void Resume () {
+		isPaused = false;
+	}
+
+	// Quit game, go to main menu
+	public void Quit () {
+		Application.LoadLevel (levelToLoad);
+	}
+}
